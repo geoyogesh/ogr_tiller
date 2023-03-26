4be0a0400478455dd3dd0d32118b2954b2240bcb
Yogesh Dhanapal <geoyogesh@gmail.com>
2023-03-26 13:42:41 -0400

    working on packaging

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ CLASSIFIERS = [
 ]
 
 setup(name='ogr_tiller',
-      version='0.0.10',
+      version='0.0.11',
       url='https://github.com/geoyogesh/ogr_tiller',
       license='GNU-GPL',
       author='Yogesh Dhanapal',
@@ -21,6 +21,6 @@ setup(name='ogr_tiller',
       long_description_content_type='text/markdown',
       zip_safe=False,
       classifiers=CLASSIFIERS,
-      install_requires=["fastapi", "uvicorn[standard]", "protobuf<=3.20.3", "parse", "mercantile", "fiona",
-                        "mapbox_vector_tile==2.0.1"]
+      install_requires=["fastapi", "uvicorn[standard]", "protobuf", "parse", "mercantile", "fiona",
+                        "mapbox_vector_tile"]
       )
