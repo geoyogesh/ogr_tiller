271e9ae709c3da270d4015ee33b47fee81783718
Yogesh Dhanapal <geoyogesh@gmail.com>
2023-03-26 12:24:19 -0400

    included cli start triggers

--- ogr_tiller/ogr_tiller.py
+++ ogr_tiller/ogr_tiller.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from entities.job_param import JobParam
 import uvicorn
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import Response
+from utils.ogr_utils import setup_ogr_cache
 from utils.fast_api_utils import TimeOutException, timeout_response
 
 import utils.ogr_utils
@@ -10,10 +12,12 @@ import utils.tile_utils as tile_utils
 import json
 import os
 
-# setup mbtile cache
-setup_cache()
 
-def start_api():
+def start_api(job_param: JobParam):
+    # setup mbtile cache
+    setup_cache(job_param.cache_folder)
+    setup_ogr_cache(job_param.data_folder)
+
     app = FastAPI()
     app.add_middleware(
         CORSMiddleware,
@@ -31,26 +35,24 @@ def start_api():
             "Cache-Control": 'no-cache, no-store'
         }
         return Response(content=json.dumps(data), headers=headers)
-    
 
     @app.get("/tilesets/{tileset}/info/tile.json")
     async def get_tileset_info(tileset: str):
         if tileset not in utils.ogr_utils.tilesets:
             return Response(status_code=404)
-        
+
         data = utils.ogr_utils.get_tile_json(tileset)
         headers = {
             "content-type": "application/json",
             "Cache-Control": 'no-cache, no-store'
         }
         return Response(content=json.dumps(data), headers=headers)
-    
 
     @app.get("/tilesets/{tileset}/tiles/{z}/{x}/{y}.mvt")
     async def get_tile(tileset: str, z: int, x: int, y: int):
         if tileset not in utils.ogr_utils.tilesets:
             return Response(status_code=404)
-        
+
         cached_data = read_cache(tileset, x, y, z)
         if cached_data is not None:
             headers = {
@@ -58,21 +60,19 @@ def start_api():
                 "Cache-Control": 'no-cache, no-store'
             }
             return Response(content=cached_data, headers=headers)
-    
-        
+
         layer_features = utils.ogr_utils.get_features(tileset, x, y, z)
         if len(layer_features) == 0:
             return Response(status_code=404)
-        
+
         data = None
         try:
             data = tile_utils.get_tile(layer_features, x, y, z)
         except TimeOutException:
             return timeout_response()
-        
 
         # update cache
-        update_cache(tileset,x, y, z, data)
+        update_cache(tileset, x, y, z, data)
 
         headers = {
             "content-type": "application/vnd.mapbox-vector-tile",
@@ -80,14 +80,16 @@ def start_api():
         }
         return Response(content=data, headers=headers)
 
-
-
     @app.get("/")
     async def index():
         return "Welcome"
-    
-    uvicorn.run(app, host="0.0.0.0", port=8080)
-    
+
+    uvicorn.run(app, host="0.0.0.0", port=int(job_param.port))
+
 
 if __name__ == "__main__":
-    start_api()
+    data_folder = './../data/'
+    cache_folder = './../cache/'
+    port = '8080'
+    job_param = JobParam(data_folder, cache_folder, port)
+    start_api(job_param)
