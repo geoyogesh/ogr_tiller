e6b493cfcc0fe3ed81f78054d43f7d877ae68b29
Yogesh Dhanapal <geoyogesh@gmail.com>
2023-03-26 18:45:06 -0400

    included flag to disable caching

--- ogr_tiller/poco/job_param.py
+++ ogr_tiller/poco/job_param.py
@@ -2,7 +2,9 @@ class JobParam:
     def __init__(self,
                  data_folder: str,
                  cache_folder: str,
-                 port: str):
+                 port: str,
+                 disable_caching: bool):
         self.data_folder = data_folder
         self.cache_folder = cache_folder
         self.port = port
+        self.disable_caching = disable_caching
