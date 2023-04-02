7b4ed25e4776bc858002a404d186972ab531a1a8
Yogesh Dhanapal <geoyogesh@gmail.com>
2023-04-01 21:50:07 -0400

    fix(bounds): improve bounds exception handling
    - Enhance error handling for bounds exceptions in ogr_utils.py
    - Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ CLASSIFIERS = [
 ]
 
 setup(name='ogr_tiller',
-      version='0.0.16',
+      version='0.0.17',
       url='https://github.com/geoyogesh/ogr_tiller',
       license='GNU-GPL',
       author='Yogesh Dhanapal',
