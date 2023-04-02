b4068b3a2bedca0ccae7b9423f25db0a8be0a4ef
Yogesh Dhanapal <geoyogesh@gmail.com>
2023-04-01 22:48:10 -0400

    handled unknown style case

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ CLASSIFIERS = [
 ]
 
 setup(name='ogr_tiller',
-      version='0.0.18',
+      version='0.0.19',
       url='https://github.com/geoyogesh/ogr_tiller',
       license='GNU-GPL',
       author='Yogesh Dhanapal',
