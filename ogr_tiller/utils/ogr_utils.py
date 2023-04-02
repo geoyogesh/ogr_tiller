22eb2d394f0c2629e8d469313be470b31046876a
Yogesh Dhanapal <geoyogesh@gmail.com>
2023-04-01 23:01:35 -0400

    fix(geometry): handle unknown geometry types
    - Add handling for unknown geometry types in ogr_utils.py
    - Increment version

--- ogr_tiller/utils/ogr_utils.py
+++ ogr_tiller/utils/ogr_utils.py
@@ -111,13 +111,13 @@ def get_starter_style(port: str) -> Any:
             with fiona.open(ds_path, 'r', layer=layer_name) as layer:
                 layer_geometry_types.append((tileset, layer_name, layer.schema['geometry']))
 
-    geometry_order = ['Point', 'MultiPoint', 'LineString', 'MultiLineString', 'Polygon', 'MultiPolygon', 'UnKnown']
+    geometry_order = ['Point', 'MultiPoint', 'LineString', 'MultiLineString', 'Polygon', 'MultiPolygon', 'Unknown']
     layer_index = 0
     for orderGeometry in geometry_order:
         color = get_color(layer_index)
         for tileset, layer_name, geometryType in layer_geometry_types:
             if orderGeometry == geometryType:
-                if geometryType == 'UnKnown':
+                if geometryType == 'Unknown':
                     for g in ['Point', 'LineString', 'Polygon']:
                         style_json['layers'].append(get_layer_style(tileset, color, f'{layer_name}_{g.lower()}', g))
                 else:
