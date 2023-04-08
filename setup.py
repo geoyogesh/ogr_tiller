a790ebc8e912d9cb66bea772e8c14e9452f08163
Yogesh Dhanapal <geoyogesh@gmail.com>
2023-04-08 01:05:36 -0400

    performance improvements

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ CLASSIFIERS = [
 ]
 
 setup(name='ogr_tiller',
-      version='0.0.36',
+      version='0.0.37',
       url='https://github.com/geoyogesh/ogr_tiller',
       license='GNU-GPL',
       author='Yogesh Dhanapal',
