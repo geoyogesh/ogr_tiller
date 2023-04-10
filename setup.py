44d52237639c488a65806b8b82b8048d45b2dd46
Yogesh Dhanapal <geoyogesh@gmail.com>
2023-04-10 00:15:32 -0400

    refactor(labels): continue polylabel implementation
    Additional work on polylabel integration in tile_utils.py.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ CLASSIFIERS = [
 ]
 
 setup(name='ogr_tiller',
-      version='0.0.41',
+      version='0.0.42',
       url='https://github.com/geoyogesh/ogr_tiller',
       license='GNU-GPL',
       author='Yogesh Dhanapal',
