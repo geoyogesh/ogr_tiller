f0e4ae6ee1bd7c52ade6d5b3a2e3560b1396df1f
Yogesh Dhanapal <geoyogesh@gmail.com>
2023-04-12 23:28:02 -0400

    perf(cache): optimize tile cache building process
    - Simplify tile generation logic in cache_builder.py
    - Optimize get_tile_descendant_tiles function in tile_utils.py
    - Remove unnecessary bounding box operations
    - Improve overall cache build performance

--- ogr_tiller/cache_builder.py
+++ ogr_tiller/cache_builder.py
@@ -1,8 +1,9 @@
+import os
 from ogr_tiller.poco.job_param import JobParam
 from ogr_tiller.poco.tileset_manifest import TilesetManifest
 from ogr_tiller.utils.job_utils import common
 from ogr_tiller.utils.monitor import timeit
-from ogr_tiller.utils.ogr_utils import get_tile_json, get_tileset_manifest, get_tilesets
+from ogr_tiller.utils.ogr_utils import get_data_location, get_tile_json, get_tileset_manifest, get_tilesets
 from ogr_tiller.utils.sqlite_utils import cleanup_mbtile_cache, update_cache, update_multiple_cache
 from shapely.geometry import box, shape, mapping
 from supermercado import edge_finder, uniontiles, burntiles, super_utils
@@ -27,26 +28,20 @@ def build_cache(job_param: JobParam):
             print(f'{tileset}: skipping tileset because it may be empty')
             progress.update(progress_tilesets_task_id, advance=1)
             return
-        bbox = box(*tilejson['bounds'])
-        fc = {
-            "features": [{"type": "Feature", "geometry": a} for a in [mapping(b) for b in [bbox]]]
-        }
-        features = [f for f in super_utils.filter_features(fc["features"])]
 
-        print(f'{tileset}: Generating seed tilelist with {tilejson["minzoom"]} level')
-        tiles = burntiles.burn(features, tilejson['minzoom'])
+        # prep
+        
+        #print('working on seed ', tileset, seed_x, seed_y, seed_z)
+        progress_task_id = progress.add_task(description=f"{tileset}", start=0, total=None)
+        ds_path = os.path.join(get_data_location(), f'{tileset}.gpkg')
+        manifest: TilesetManifest = get_tileset_manifest()[tileset]
+        layer_features, srid = tile_utils.get_all_features(ds_path)
+        
+
 
         result = []
         
-        progress_task_id = progress.add_task(description=f"{tileset}", total=None)
-        def process_tile(tile):
-            x, y, z = tile
-            x = x.item()
-            y = y.item()
-            z = z.item()
-            tile_utils.get_tile_descendant_tiles(tileset, x, y, z, tilejson["maxzoom"], result, progress, progress_task_id)
-        for tile in tiles:
-            process_tile(tile)
+        tile_utils.get_tile_descendant_tiles(tileset, layer_features, 0, 0, 0, manifest, srid, tilejson["minzoom"], tilejson["maxzoom"], result, progress, progress_task_id)
         progress.update(progress_task_id, total=len(result))
         progress.update(progress_tilesets_task_id, advance=1)
         
